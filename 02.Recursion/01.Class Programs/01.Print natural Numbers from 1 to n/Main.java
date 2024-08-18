efc6eeb0c8b9caece78ff0f25b0c37e6188deecd
Yogesh <yp901981@gmail.com>
2024-08-17 18:59:32 -0700

    added new program

---
+++ 02.Recursion/01.Class Programs/01.Print natural Numbers from 1 to n/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Demo{
+    static void print(int n){
+        if(n>=1){
+            // System.out.println(n+" "); //Reverse order
+            print(n-1);
+            System.out.println(n+" ");
+        }
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter n value:");
+        int n=sc.nextInt();
+        Demo.print(n);
+        sc.close();
+    }
+}
