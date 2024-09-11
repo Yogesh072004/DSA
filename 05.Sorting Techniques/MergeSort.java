5ef7c602fa2cff0870d258e1023d0f33d6e3f3ce
Yogesh <yp@gmail.com>
2024-09-11 22:44:28 +0530

    added

---
+++ 05.Sorting Techniques/MergeSort.java
@@ -0,0 +1,50 @@
+public class MergeSort {
+    public static void main(String[] args) {
+        int a[]={54,6,8,35,7,68,29};
+        mergesort(a,0,a.length-1);
+        for(int num:a) {
+            System.out.println(num+" ");
+        }
+
+    }
+
+    private static void mergesort(int[] a, int low, int high) {
+        int left=low;
+        int right=high;
+        int mid;
+        if(left<right){
+            mid=(left+right)/2;
+            mergesort(a, low, mid);
+            mergesort(a, mid+1, high);
+            merge(a,low,mid,high);
+
+        }
+
+    }
+
+    private static void merge(int[] a, int left, int mid, int right) {
+        int n=a.length;
+       int i,j,k;
+       int b[]=new int[n];
+       i=left;
+       j=mid+1;
+       k=right;
+       while (i<=mid && j<=right) {
+        if(a[i]<=a[j]){
+            b[k++]=a[i++];
+        }else{
+            b[k++]=a[j++];
+        }
+       }
+       while (i<=mid) {
+        b[k++]=a[i++];
+       }
+       while ((j<=right)) {
+        b[k++]=a[j++];
+       }
+       for(i=left;i<=right;i++){
+        a[i]=b[i];
+       }
+
+    }
+}
