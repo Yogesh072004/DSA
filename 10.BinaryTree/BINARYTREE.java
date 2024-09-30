0640ec8577a8a9d83827036f6bfe41eba7d215ef
Yogesh <yp@gmail.com>
2024-09-30 22:06:39 +0530

    added

---
+++ 10.BinaryTree/BINARYTREE.java
@@ -0,0 +1,21 @@
+class BST{
+    static int  index=-1;
+
+    class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node (int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+}
+public class BINARYTREE {
+    public static void main(String[] args) {
+        
+    }
+}
